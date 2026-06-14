0f86ff8f366a2073487b5cf86dce1476dcc4b91c
DANIEL DE FREITAS ESTEVES DA COSTA <143741099+BlackDani557@users.noreply.github.com>
2026-06-14 18:48:51 -0300

    atualização - tela inicial adm
    troquei a foto por um icon users(svg)

--- index.php
+++ index.php
@@ -4,6 +4,26 @@
 
     switch($paginas)
     {
+        case 'Compra':
+            include './Views/Geral/Compra.html';
+        break;
+
+        case 'Venda':
+            include './Views/Geral/Vender.html';
+        break;
+
+        case 'Financiamento':
+            include './Views/Geral/Financiamento.html';
+        break;
+
+        case 'TabelaFIPE':
+            include './Views/Geral/TabelaFIPE.html';
+        break;
+
+        case 'Catalogo0KM':
+            include './Views/Geral/Catalogo0km.html';
+        break;
+
         default:
             include './Views/Geral/TelaInicial.html';
         break;
